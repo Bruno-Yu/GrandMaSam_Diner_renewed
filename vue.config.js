0ebe8c69dd60b3d6d5f36359454d2338445432b3
Bruno-Yu <jackhellowin@gmail.com>
2022-05-28 16:36:10 +0800

    adjusted

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,3 @@
 module.exports = {
-  publicPath:
-    process.env.NODE_ENV === 'production' ? '/grandmasam_diner_renewed/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
 };
